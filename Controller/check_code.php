5fbf08cacb45ff28a3f0db8dc8be63f220ba52e3
mohamedbenhariz315 <mohamedbenhariz8@gmail.com>
2025-12-11 10:09:09 +0100

    Ajout des metier (some improvements)

--- Controller/check_code.php
+++ Controller/check_code.php
@@ -8,13 +8,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     $pdo = config::getConnexion();
     
-    // Vérifier si le code correspond à l'email en session
+
     $stmt = $pdo->prepare("SELECT * FROM user WHERE email = :email AND verification_code = :code");
     $stmt->execute(['email' => $email, 'code' => $userCode]);
     $user = $stmt->fetch();
 
     if ($user) {
-        // Code bon ! On autorise l'accès à la page de changement de MDP
+       
         $_SESSION['code_verified'] = true;
         header("Location: ../View/FRONT OFFICE/PRINCIPAL/genifty-html/reset_password.php");
         exit();
